27cc298b2a2950ad34a75c561a4c2438bf380a20
tetesuhito.fujimoto <fujitetsu1021@gmail.com>
2020-09-06 06:18:01 +0000

    post

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,8 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @tasklists = @user.tasklists.order(id: :desc).page(params[:page])
+    counts(@user)
   end
 
   def new
