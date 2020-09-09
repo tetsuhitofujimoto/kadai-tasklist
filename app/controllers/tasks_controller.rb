bffb7ee8ce05fcb90c0e38c041478048362773bd
tetesuhito.fujimoto <fujitetsu1021@gmail.com>
2020-09-09 09:58:58 +0000

    saiteisyutsu2

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -16,11 +16,11 @@ class TasksController < ApplicationController
   def create
     @task = current_user.tasks.build(task_params)
     if @task.save
-      flash[:success] = 'メッセージを投稿しました。'
+      flash[:success] = 'タスクを作成しました。'
       redirect_to root_url
     else
       @tasks = current_user.tasks.order(id: :desc).page(params[:page])
-      flash.now[:danger] = 'メッセージの投稿に失敗しました。'
+      flash.now[:danger] = 'タスクの作成に失敗しました。'
       render :new
     end
   end
@@ -30,17 +30,17 @@ class TasksController < ApplicationController
 
   def update
     if @task.update(task_params)
-      flash[:success] = "Task は正常に更新されました"
+      flash[:success] = "タスクは正常に更新されました。"
       redirect_to @task
     else
-      flash[:danger] = "Task は更新されませんでした"
+      flash[:danger] = "タスクは更新されませんでした。"
       render :edit
     end 
   end
 
   def destroy
     @task.destroy
-    flash[:success] = 'メッセージを削除しました。'
+    flash[:success] = 'タスクを削除しました。'
     redirect_back(fallback_location: root_path)
   end
   
